bddd5239ca1c80da45161feed5430c3463dceea0
Riobener <venisonsheff@gmail.com>
2021-07-18 05:03:48 +0300

    Starter init added.

--- src/main/kotlin/com/riobener/samplemanagerservice/SampleManagerServiceApplication.kt
+++ src/main/kotlin/com/riobener/samplemanagerservice/SampleManagerServiceApplication.kt
@@ -1,11 +1,23 @@
 package com.riobener.samplemanagerservice
 
+import com.riobener.samplemanagerservice.service.StorageService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 
 @SpringBootApplication
 class SampleManagerServiceApplication
 
 fun main(args: Array<String>) {
 	runApplication<SampleManagerServiceApplication>(*args)
+}
+@Autowired
+lateinit var fileStorage: StorageService;
+
+@Bean
+fun run() = CommandLineRunner {
+	fileStorage.deleteAll()
+	fileStorage.init()
 }
